eb2bcb66a566f6c6e823e67f63b4dcc76e9bc8c5
lynchdj <darcy.lynch27@gmail.com>
2021-01-29 16:47:43 +1100

    Added ESJ

--- app.js
+++ app.js
@@ -7,18 +7,43 @@ const PORT = 3000;
 // Express application
 const app = express();
 
+// Register view engine
+app.set("view engine", "ejs");
+
 // Listen for requests
 app.listen(PORT);
 
 // Get index
 app.get("/", (req, res) => {
   //res.send("<p>Homepage</p>"); // send() automatically infers the correct header
-  res.sendFile("./views/index.html", { root: __dirname });
+  //res.sendFile("./views/index.html", { root: __dirname }); // Send an entire file to client
+
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+
+  res.render("index", { title: "Home", blogs }); // Render a page using view engine and send to client
 });
 
 // Get about
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
+});
+
+// Get about
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
 });
 
 // Redirect about-us
@@ -28,5 +53,5 @@ app.get("/about-us", (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "404" });
 });
